1c847e6730f9687a1f1515c60b589a0eeb78e993
Matheus <msgoncalves@inf.ufpel.edu.br>
2025-04-10 04:23:57 -0300

    Criação do componente do Logotipo

---
+++ src/components/AppLogotype.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  import imgSrc from "@/assets/logotype.png"
+</script>
+
+<template>
+  <div class="d-flex justify-content-center w-full">
+    <img
+      :src="imgSrc"
+      alt="Logotipo do PokéScan"
+      class="w-100"
+    />
+  </div>
+</template>
+
+<style scoped>
+</style>
