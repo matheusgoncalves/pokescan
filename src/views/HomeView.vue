3eb9f916772f85838cfde1092551f34008040cf9
Matheus <msgoncalves@inf.ufpel.edu.br>
2025-04-11 18:15:40 -0300

    Configurando redirecionamento dos cards de pokémons para a página de detalhes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -255,23 +255,25 @@
 
   <div class="d-flex flex-wrap justify-content-center">
     <div v-for="pokemon in pokemons" :key="pokemon.order" class="d-flex flex-column border border-3 border-solid border-secondary rounded-5 p-3 m-3">
-      <div class="d-flex justify-content-center align-items-end p-4 bg-secondary-subtle rounded-5" style="width: 168px; min-width: 168px; max-width: 160px; height: 160px;">
-        <img
-          v-if="pokemon.sprites?.other?.['showdown']?.front_default"
-          :src="pokemon.sprites?.other?.['showdown']?.front_default"
-          :alt="pokemon.name"
-          style="max-width: 100%; max-height: 100%;"
-        />
-      </div>
-
-      <div class="mt-3">
-        <p>#{{ String(pokemon.id).padStart(4, '0') }}</p>
-        <p class="text-capitalize text-nowrap text-truncate" style="max-width: 120px;">{{ pokemon.name }}</p>
-
-        <div v-for="type in pokemon.types" :key="type.type.name" class="d-inline-flex align-items-center">
-          <p class="text-capitalize px-3 py-1 me-2 mb-0 rounded-5 bg-dark-subtle" style="font-size: 14px;">{{ type.type.name }}</p>
+      <router-link :to="`/pokemons/${pokemon.name}`" class="text-dark text-decoration-none">
+        <div class="d-flex justify-content-center align-items-end p-4 bg-secondary-subtle rounded-5" style="width: 168px; min-width: 168px; max-width: 160px; height: 160px;">
+          <img
+            v-if="pokemon.sprites?.other?.['showdown']?.front_default"
+            :src="pokemon.sprites?.other?.['showdown']?.front_default"
+            :alt="pokemon.name"
+            style="max-width: 100%; max-height: 100%;"
+          />
         </div>
-      </div>
+
+        <div class="mt-3">
+          <p>#{{ String(pokemon.id).padStart(4, '0') }}</p>
+          <p class="text-capitalize text-nowrap text-truncate" style="max-width: 120px;">{{ pokemon.name }}</p>
+
+          <div v-for="type in pokemon.types" :key="type.type.name" class="d-inline-flex align-items-center">
+            <p class="text-capitalize px-3 py-1 me-2 mb-0 rounded-5 bg-dark-subtle" style="font-size: 14px;">{{ type.type.name }}</p>
+          </div>
+        </div>
+      </router-link>
     </div>
   </div>
 
